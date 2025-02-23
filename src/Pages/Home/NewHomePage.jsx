b7641a82eb7827190fec5fb7a70aae46f8cbbb1e
basantsaed2 <basantsaed718@gmail.com>
2025-02-23 15:28:17 +0200

    commit

--- src/Pages/Home/NewHomePage.jsx
+++ src/Pages/Home/NewHomePage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import RedLogo from '../../assets/Images/RedLogo'
 import MenuIcon from "../../assets/Icons/MenuIcon";
 import DashIcon from "../../assets/Icons/DashIcon";
 import AppleIcon from "../../assets/Icons/AppleIcon";
@@ -13,7 +12,6 @@ const NewHomePage=()=>{
          <div className={`w-full h-full flex flex-col md:flex-row pb-0 p-2 md:p-6 justify-center`}>
 
             <div className={`w-full md:w-1/2 h-full flex flex-col items-center`}>
-                {/* <RedLogo width={180} height={180} /> */}
                 <img src={lamadaLogo} width={180} height={180} alt="Main Logo" />
                  <div className="flex items-center justify-center gap-2">
                     <h1 className="text-mainColor font-semibold text-2xl">Lamada</h1>
@@ -26,20 +24,18 @@ const NewHomePage=()=>{
                         <MenuIcon/>
                         <h1 className="text-2xl text-mainColor">Menu</h1>
                     </Link>
-                    <Link to="/menu" className="bg-[#9E090F1A] flex flex-col gap-3  items-center justify-center rounded-xl p-2 md:p-6">
+                    <Link target="_blank" to="https://www.restaurantlogin.com/ordering/restaurant/menu?company_uid=a2fc19d6-e9ea-4b4d-8ae9-526bac2e6b97&restaurant_uid=8ac9c3d9-c2d3-4d26-b397-c0fb409535a7&facebook=true" className="bg-[#9E090F1A] flex flex-col gap-3  items-center justify-center rounded-xl p-2 md:p-6">
                         <DashIcon/>
                         <h1 className="text-2xl text-mainColor">Order Now</h1>
                     </Link>      
                 </div>
 
                 <div className="flex gap-1 p-4 pb-0">
-                    <Link>
+                    <Link to="#" className="opacity-50 cursor-not-allowed">
                         <GooglePlayIcon/>
-                        {/* <h1 className="text-2xl text-mainColor">Menu</h1> */}
                     </Link>
-                    <Link>
+                    <Link to="https://apps.apple.com/eg/app/lamada/id6553989792">
                         <AppleIcon/>
-                        {/* <h1 className="text-2xl text-mainColor">Order Now</h1> */}
                     </Link>      
                 </div>
             </div>
--- src/Pages/Menu/NewMenuPage.jsx
+++ src/Pages/Menu/NewMenuPage.jsx
@@ -3,12 +3,20 @@ import m1 from "../../assets/Images/m1.jpg";
 import m2 from "../../assets/Images/m2.jpg";
 import m3 from "../../assets/Images/m3.jpg";
 import m4 from "../../assets/Images/m.jpeg";
-import { AiOutlineClose } from "react-icons/ai"; // Importing the close icon
+import { AiOutlineClose, AiOutlineLeft, AiOutlineRight } from "react-icons/ai"; // Importing icons
 
 const menuImages = [m1, m2, m3, m4];
 
 const NewMenuPage = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const handlePrev = () => {
+    setSelectedIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : menuImages.length - 1));
+  };
+
+  const handleNext = () => {
+    setSelectedIndex((prevIndex) => (prevIndex < menuImages.length - 1 ? prevIndex + 1 : 0));
+  };
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
@@ -18,23 +26,40 @@ const NewMenuPage = () => {
             src={src}
             alt={`menu-item-${index}`}
             className="w-full h-auto cursor-pointer transition-transform duration-300 hover:scale-110"
-            onClick={() => setSelectedImage(src)}
+            onClick={() => setSelectedIndex(index)}
           />
         </div>
       ))}
 
       {/* Fullscreen Image View */}
-      {selectedImage && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50">
+      {selectedIndex !== null && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50 p-6 md:p-8">
           {/* Close Button */}
           <button
             className="absolute top-5 right-5 bg-white text-black p-2 rounded-full shadow-lg hover:bg-gray-300 transition"
-            onClick={() => setSelectedImage(null)}
+            onClick={() => setSelectedIndex(null)}
           >
-            <AiOutlineClose size={24} />
+            <AiOutlineClose size={24} className="text-mainColor"/>
           </button>
 
-          <img src={selectedImage} className="max-w-full max-h-full rounded-lg" alt="Enlarged" />
+          {/* Previous Button */}
+          <button
+            className="absolute left-1 xl:left-24 bg-white text-black p-1 rounded-full shadow-lg hover:bg-gray-300 transition"
+            onClick={handlePrev}
+          >
+            <AiOutlineLeft size={20} className="text-mainColor" />
+          </button>
+
+          {/* Display Image */}
+          <img src={menuImages[selectedIndex]} className="max-w-full max-h-full rounded-lg" alt="Enlarged" />
+
+          {/* Next Button */}
+          <button
+            className="absolute right-1 xl:right-24 bg-white text-black p-1 rounded-full shadow-lg hover:bg-gray-300 transition"
+            onClick={handleNext}
+          >
+            <AiOutlineRight size={20} className="text-mainColor"/>
+          </button>
         </div>
       )}
     </div>
